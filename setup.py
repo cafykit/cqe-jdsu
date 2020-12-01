1e5cdc07e474255afd6441f3e66257171fcd9e45
manali-gup <75265965+manali-gup@users.noreply.github.com>
2020-12-01 13:01:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setuptools.setup(
-    name = "cqe_jdsu",
+    name = "cqe-jdsu",
     version = "0.0.1",
     author = "Manali Gupta",
     author_email = "manligup@cisco.com",
